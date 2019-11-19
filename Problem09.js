1deef9c42dd3795c0f3404ec9abe35dd72c1eec2
Stephen McCready <stephenmccready@gmail.com>
2019-11-19 16:09:10 -0500

    Update and rename Problem9.js to Problem09.js
    Added problem statement

--- Problem09.js
+++ Problem09.js
@@ -1,3 +1,12 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
 var a=3, b=4, c=5, productABC=0;
 for(a=3; a<1000; a++) {
 	var aSQ = a * a;
