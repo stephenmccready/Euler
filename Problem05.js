72c965c0456145a32ff3f564d88637c27e64557a
Stephen McCready <stephenmccready@gmail.com>
2019-11-19 16:11:26 -0500

    Update and rename Problem5.js to Problem05.js

--- Problem05.js
+++ Problem05.js
@@ -1,3 +1,8 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
 var start = 20 * 19;
 var isFound = 'N';
 console.log(start);
